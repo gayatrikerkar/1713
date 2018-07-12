a4ab109d4917ca915cffc1e823f1dd3e11bd7737
gayatri <gayatrikerkar11@gmail.com>
2018-07-12 16:11:02 +0530

    updated bubble sort:accessing class methods without creating its objects

--- Sorting/src/me/App.java
+++ Sorting/src/me/App.java
@@ -5,8 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int d[] = {6,1,5,2};
-  Bubble bsort = new Bubble(d);
-bsort.sort();
+  //Bubble bsort = new Bubble(d);
+Bubble.sort(d);
 	}
 
 }
--- Sorting/src/me/Bubble.java
+++ Sorting/src/me/Bubble.java
@@ -1,10 +1,10 @@
 package me;
 
-public class Bubble {
+public  class Bubble {
 
 	//public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   int data[] = null;//{3,5,12,11,7,9,2};
+ int data[] = null;//{3,5,12,11,7,9,2};
    
    
    
@@ -30,33 +30,33 @@ public Bubble(int[] data) {
 
 
 
-public void sort()
+public static void sort(int array[])
    {
-   int i,j,temp,n=data.length;
-    System.out.println("bubble sort");
-    for(i=0;i<n;i++)
-    {
-   	 System.out.println(data[i]);
-    }
-     
-      for(i=0;i<n-1;i++)
-      {
-    	for(j=0;j<n-i-1;j++)  
-    	{
-    		if(data[j] > data[j+1])
-    		{
-    			temp=data[j];
-    			data[j]=data[j+1];
-    			data[j+1]=temp;
-    		}
-    	}
-      }
-      System.out.println("sorted array");
-     for(i=0;i<n;i++)
-     {
-    	 System.out.println(data[i]);
-     }
-	}
-
+			   int i,j,temp,n=array.length;
+			    System.out.println("bubble sort");
+			    for(i=0;i<n;i++)
+			    {
+			   	 System.out.println(array[i]);
+			    }
+			     
+			      for(i=0;i<n-1;i++)
+			      {
+			    	for(j=0;j<n-i-1;j++)  
+			    	{
+			    		if(array[j] > array[j+1])
+			    		{
+			    			temp=array[j];
+			    			array[j]=array[j+1];
+			    			array[j+1]=temp;
+			    		}
+			    	}
+			      }
+			      System.out.println("sorted array");
+			     for(i=0;i<n;i++)
+			     {
+			    	 System.out.println(array[i]);
+			     }
+				}
+			
 
 } 
