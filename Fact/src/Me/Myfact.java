dd27d787ef7d1c35e238995d8681dd1f26f62158
gayatri <gayatrikerkar11@gmail.com>
2018-07-17 15:04:28 +0530

    factorial

---
+++ Fact/src/Me/Myfact.java
@@ -0,0 +1,16 @@
+package Me;
+
+public class Myfact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("factorial program");
+	       int i,fact=1;
+	       for(i=5;i>0;i--)
+	       {
+	    	  fact=fact*i;
+	       }
+	       System.out.println(fact);
+	}
+
+}
