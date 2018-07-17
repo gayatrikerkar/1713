946d671479b91bc06053612e13a6be49a86d98b1
gayatri <gayatrikerkar11@gmail.com>
2018-07-17 15:18:54 +0530

    2nd part

--- Fact/src/Me/Myfact.java
+++ Fact/src/Me/Myfact.java
@@ -4,13 +4,10 @@ public class Myfact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("factorial program");
-	       int i,fact=1;
-	       for(i=5;i>0;i--)
-	       {
-	    	  fact=fact*i;
-	       }
-	       System.out.println(fact);
+	 Facto my = new Facto();
+	 my.setVar(5);
+	 System.out.println("var:"+ ""+my.getVar());
+	  my.calc(7);
 	}
 
 }
