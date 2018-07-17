74804d1d5b6a7a9b1221560dbc5535761e4262e4
gayatri <gayatrikerkar11@gmail.com>
2018-07-17 15:51:02 +0530

    3rd commit

--- Fact/src/Me/Myfact.java
+++ Fact/src/Me/Myfact.java
@@ -4,10 +4,12 @@ public class Myfact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	 Facto my = new Facto();
-	 my.setVar(6);
-	 System.out.println("var:"+ ""+my.getVar());
-	  my.calc(6);
+	 //Facto my = new Facto();
+	 //my.setVar(6);
+	 //System.out.println("var:"+ ""+my.getVar());
+	//  my.calc(6);
+		int d=7;
+		Facto.calc(d);
 	}
 
 }
